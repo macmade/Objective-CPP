65f7f841d85728cee1ac0fed757a1d286c89d509
macmade <macmade@xs-labs.com>
2015-09-04 01:50:24 +0200

    Travis...

--- Scripts/travis-after.sh
+++ Scripts/travis-after.sh
@@ -5,4 +5,4 @@ source Scripts/xcenv-lib.sh
 declare -r DIR_BUILD_LIB="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
 source Scripts/xcenv-tests.sh
 declare -r DIR_BUILD_TESTS="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
-xcode-coveralls --include Objective-CPP --project Objective-CPP.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}"
+xcode-coveralls --include ObjectiveCPP --project ObjectiveCPP.xcodeproj "${DIR_BUILD_LIB}" "${DIR_BUILD_TESTS}"
