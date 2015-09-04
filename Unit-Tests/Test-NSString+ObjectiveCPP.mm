361ea2fb3b5c4b5df49d976ea575b015c437ac3b
macmade <macmade@xs-labs.com>
2015-09-04 02:09:34 +0200

    Placeholders for unit tests...

--- Unit-Tests/Test-NSString+ObjectiveCPP.mm
+++ Unit-Tests/Test-NSString+ObjectiveCPP.mm
@@ -38,7 +38,210 @@
 
 using namespace testing;
 
-TEST( NSString_ObjectiveCPP, test )
-{
-    ASSERT_TRUE( true );
-}
+TEST( Objective_CPP, defaultCPPStringEncoding )
+{}
+
+TEST( Objective_CPP, setDefaultCPPStringEncoding )
+{}
+
+TEST( Objective_CPP, stringWithCPPString )
+{}
+
+TEST( Objective_CPP, stringWithCPPString_encoding )
+{}
+
+TEST( Objective_CPP, cppStringWithContentsOfFile_encoding_error )
+{}
+
+TEST( Objective_CPP, cppStringWithContentsOfFile_usedEncoding_error )
+{}
+
+TEST( Objective_CPP, cppStringWithContentsOfURL_encoding_error )
+{}
+
+TEST( Objective_CPP, cppStringWithContentsOfURL_usedEncoding_error )
+{}
+
+TEST( Objective_CPP, initWithCPPString )
+{}
+
+TEST( Objective_CPP, initWithCPPString_encoding )
+{}
+
+TEST( Objective_CPP, cppString )
+{}
+
+TEST( Objective_CPP, cppStringUsingEncoding )
+{}
+
+TEST( Objective_CPP, cppStringByAppendingString )
+{}
+
+TEST( Objective_CPP, stringByAppendingCPPString )
+{}
+
+TEST( Objective_CPP, cppStringByPaddingToLength_withString_startingAtIndex )
+{}
+
+TEST( Objective_CPP, stringByPaddingToLength_withCPPString_startingAtIndex )
+{}
+
+TEST( Objective_CPP, cppStringByTrimmingCharactersInSet )
+{}
+
+TEST( Objective_CPP, cppSubstringFromIndex )
+{}
+
+TEST( Objective_CPP, cppSubstringWithRange )
+{}
+
+TEST( Objective_CPP, cppSubstringToIndex )
+{}
+
+TEST( Objective_CPP, rangeOfCPPString )
+{}
+
+TEST( Objective_CPP, rangeOfCPPString_options )
+{}
+
+TEST( Objective_CPP, rangeOfCPPString_options_range )
+{}
+
+TEST( Objective_CPP, rangeOfCPPString_options_range_locale )
+{}
+
+TEST( Objective_CPP, cppStringByReplacingOccurrencesOfString_withString )
+{}
+
+TEST( Objective_CPP, cppStringByReplacingOccurrencesOfString_withString_options_range )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfString_withCPPString )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfString_withCPPString_options_range )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfCPPString_withString )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfCPPString_withString_options_range )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfCPPString_withCPPString )
+{}
+
+TEST( Objective_CPP, stringByReplacingOccurrencesOfCPPString_withCPPString_options_range )
+{}
+
+TEST( Objective_CPP, cppStringByReplacingCharactersInRange_withString )
+{}
+
+TEST( Objective_CPP, stringByReplacingCharactersInRange_withCPPString )
+{}
+
+TEST( Objective_CPP, caseInsensitiveCompareWithCPPString )
+{}
+
+TEST( Objective_CPP, localizedCaseInsensitiveCompareWithCPPString )
+{}
+
+TEST( Objective_CPP, compareWithCPPString )
+{}
+
+TEST( Objective_CPP, localizedCompareWithCPPString )
+{}
+
+TEST( Objective_CPP, compareWithCPPString_options )
+{}
+
+TEST( Objective_CPP, compareWithCPPString_options_range )
+{}
+
+TEST( Objective_CPP, compareWithCPPString_options_range_locale )
+{}
+
+TEST( Objective_CPP, localizedStandardCompareWithCPPString )
+{}
+
+TEST( Objective_CPP, hasCPPStringPrefix )
+{}
+
+TEST( Objective_CPP, hasCPPStringSuffix )
+{}
+
+TEST( Objective_CPP, isEqualToCPPString )
+{}
+
+TEST( Objective_CPP, cppStringByFoldingWithOptions_locale )
+{}
+
+TEST( Objective_CPP, commonPrefixWithCPPString_options )
+{}
+
+TEST( Objective_CPP, commonCPPPrefixWithString_options )
+{}
+
+TEST( Objective_CPP, capitalizedCPPString )
+{}
+
+TEST( Objective_CPP, lowercaseCPPString )
+{}
+
+TEST( Objective_CPP, uppercaseCPPString )
+{}
+
+TEST( Objective_CPP, decomposedCPPStringWithCanonicalMapping )
+{}
+
+TEST( Objective_CPP, decomposedCPPStringWithCompatibilityMapping )
+{}
+
+TEST( Objective_CPP, precomposedCPPStringWithCanonicalMapping )
+{}
+
+TEST( Objective_CPP, precomposedCPPStringWithCompatibilityMapping )
+{}
+
+TEST( Objective_CPP, lastPathComponentAsCPPString )
+{}
+
+TEST( Objective_CPP, pathExtensionAsCPPString )
+{}
+
+TEST( Objective_CPP, cppStringByAbbreviatingWithTildeInPath )
+{}
+
+TEST( Objective_CPP, cppStringByAppendingPathComponent )
+{}
+
+TEST( Objective_CPP, cppStringByAppendingCPPPathComponent )
+{}
+
+TEST( Objective_CPP, cppStringByAppendingPathExtension )
+{}
+
+TEST( Objective_CPP, stringByAppendingCPPPathExtension )
+{}
+
+TEST( Objective_CPP, cppStringByDeletingLastPathComponent )
+{}
+
+TEST( Objective_CPP, cppStringByDeletingPathExtension )
+{}
+
+TEST( Objective_CPP, cppStringByExpandingTildeInPath )
+{}
+
+TEST( Objective_CPP, cppStringByResolvingSymlinksInPath )
+{}
+
+TEST( Objective_CPP, cppStringByStandardizingPath )
+{}
+
+TEST( Objective_CPP, cppStringByAddingPercentEscapesUsingEncoding )
+{}
+
+TEST( Objective_CPP, cppStringByReplacingPercentEscapesUsingEncoding )
+{}
+
